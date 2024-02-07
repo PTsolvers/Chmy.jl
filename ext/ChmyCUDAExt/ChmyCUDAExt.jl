25c572943367e00903d562a8a846075691a12f8d
Ludovic Raess <ludovic.rass@gmail.com>
2024-02-07 22:24:52 +0100

    Add device to ext

--- ext/ChmyCUDAExt/ChmyCUDAExt.jl
+++ ext/ChmyCUDAExt/ChmyCUDAExt.jl
@@ -1,9 +1,19 @@
 module ChmyCUDAExt
 
-using CUDA, KernelAbstractions, Chmy
+using CUDA, CUDA.CUDAKernels, KernelAbstractions, Chmy
+
+import Chmy.Architectures: heuristic_groupsize, set_device!, get_device
 
 Base.unsafe_wrap(::CUDABackend, ptr::CuPtr, dims) = unsafe_wrap(CuArray, ptr, dims)
 
 Chmy.pointertype(::CUDABackend, T::DataType) = CuPtr{T}
+
+set_device!(dev::CuDevice) = CUDA.device!(dev)
+
+get_device(::CUDABackend, id::Integer) = CuDevice(id - 1)
+
+heuristic_groupsize(::CuDevice, ::Val{1}) = (256,)
+heuristic_groupsize(::CuDevice, ::Val{2}) = (32, 8)
+heuristic_groupsize(::CuDevice, ::Val{3}) = (32, 8, 1)
 
 end
