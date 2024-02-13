ab211fd69958178f6a36104c278d29f3f203d936
Ivan Utkin <iutkin@ethz.ch>
2024-02-13 17:14:53 +0100

    Add KernelLaunch

--- src/KernelLaunch.jl
+++ src/KernelLaunch.jl
@@ -1,8 +1,9 @@
 module KernelLaunch
 
-export Launcher
-export Offset
+export Launcher, Offset
+export worksize, outer_width, inner_worksize, inner_offset, outer_worksize, outer_offset
 
+using Chmy
 using Chmy.Architectures
 using Chmy.Grids
 using Chmy.BoundaryConditions
@@ -19,84 +20,99 @@ Base.:+(::Offset{O1}, ::Offset{O2}) where {O1,O2} = Offset((O1 .+ O2)...)
 Base.:+(::Offset{O}, tp::Tuple{Vararg{Integer}}) where {O} = O .+ tp
 Base.:+(tp::Tuple{Vararg{Integer}}, off::Offset) = off + tp
 
-struct Launcher{InnerWorksize,
-                InnerOffset,
-                OuterWorkSizes,
-                OuterOffsets,
-                OuterWidth,
-                Workers}
+struct Launcher{Worksize,OuterWidth,Workers}
     workers::Workers
 end
+
+# worksize for the last dimension N takes into account only last outer width W[N], N-1 uses W[N] and W[N-1], N-2 uses W[N], W[N-1], and W[N-2]
 
 function Launcher(arch, grid; outer_width=nothing)
     worksize = size(grid, Center()) .+ 2
 
-    if isnothing(outer_width)
-        inner_worksize  = worksize
-        inner_offset    = Offset()
-        outer_worksizes = nothing
-        outer_offsets   = nothing
-        workers         = nothing
+    if !isnothing(outer_width)
+        setup() = activate!(arch; priority=:high)
+        workers = ntuple(Val(ndims(grid))) do _
+            Base.@_inline_meta
+            ntuple(_ -> Worker(; setup), Val(2))
+        end
     else
-        # TODO
+        workers = nothing
     end
 
-    return Launcher{inner_worksize,inner_offset,outer_worksizes,outer_offsets,typeof(workers)}(workers)
+    return Launcher{worksize,outer_width,typeof(workers)}(workers)
 end
 
-Base.@assume_effects :foldable Base.ndims(::Launcher{WorkSize}) where {} = length(WorkSize)
-Base.@assume_effects :foldable inner_worksize(::Launcher{InnerWorkSize}) where {InnerWorkSize} = InnerWorkSize
+Base.@assume_effects :foldable Base.ndims(::Launcher{WorkSize}) where {WorkSize} = length(WorkSize)
+Base.@assume_effects :foldable worksize(::Launcher{WorkSize}) where {WorkSize} = WorkSize
+Base.@assume_effects :foldable outer_width(::Launcher{WorkSize,OuterWidth}) where {WorkSize,OuterWidth} = OuterWidth
 
-Base.@assume_effects :foldable function outer_worksize(::Launcher{IWS,IO,OuterWorkSizes}, ::Dim{D}) where {IWS,IO,OuterWorkSizes,D}
-    return InnerWorkSize[D]
+Base.@assume_effects :foldable inner_worksize(launcher::Launcher) = worksize(launcher) .- 2 .* outer_width(launcher)
+Base.@assume_effects :foldable inner_offset(launcher::Launcher) = outer_width(launcher)
+
+Base.@assume_effects :foldable function outer_worksize(launcher::Launcher, ::Dim{D}) where {D}
+    ntuple(Val(ndims(launcher))) do I
+        Base.@_inline_meta
+        if I < D
+            worksize(launcher)[I]
+        elseif I == D
+            outer_width(launcher)[I]
+        else
+            worksize(launcher)[I] - 2outer_width(launcher)[I]
+        end
+    end
 end
 
-Base.@assume_effects :foldable function inner_offset(::Launcher{IWS,InnerOffset}) where {IWS,InnerOffset}
-    return InnerOffset
+Base.@assume_effects :foldable function outer_offset(launcher::Launcher, ::Dim{D}, ::Side{S}) where {D,S}
+    ntuple(Val(ndims(launcher))) do I
+        Base.@_inline_meta
+        if I < D
+            0
+        elseif I == D
+            S == 1 ? 0 : worksize(launcher)[I] - outer_width(launcher)[I]
+        else
+            outer_width(launcher)[I]
+        end
+    end
 end
 
-Base.@assume_effects :foldable function outer_offset(::Launcher{IWS,IO,OWS,OuterOffset}, ::Dim{D}, ::Side{S}) where {IWS,IO,OWS,OuterOffset}
-    return OuterOffset[D][S]
-end
-
-Base.@assume_effects :foldable outer_width(::Launcher{IWS,IO,OWS,OO,OuterWidth}) where {IWS,IO,OWS,OO,OuterWidth} = OuterWidth
-
-function (l::Launcher)(arch::Architecture, grid::StructuredGrid, kernel::F, args...;
-                       bc=nothing, async=false) where {F}
-    backend   = Chmy.get_backend(arch)
-    groupsize = heuristic_groupsize(backend, Val(ndims(l)))
-
-    offset = Offset(-1, -1)
+function (launcher::Launcher)(arch::Architecture, grid, kernel::F, args...; bc=nothing, async=false) where {F}
+    backend = Architectures.get_backend(arch)
+    offset  = Offset(-1)
 
     if isnothing(bc)
-        worksize = size(grid, Center()) .+ 2
-        fun = kernel(backend, groupsize, worksize)
-        fun(args..., offset)
+        launch_without_bc(backend, launcher, offset, kernel, args...)
     else
+        launch_with_bc(arch, grid, launcher, offset, kernel, bc, args...)
     end
 
     async || KernelAbstractions.synchronize(backend)
+    return
 end
 
-@inline function launch_without_bc(backend, worksize, groupsize, offset, kernel, args...)
-    fun = kernel(backend, groupsize, worksize)
+@inline function launch_without_bc(backend, launcher, offset, kernel, args...)
+    groupsize = heuristic_groupsize(backend, Val(ndims(launcher)))
+    fun = kernel(backend, groupsize, worksize(launcher))
     fun(args..., offset)
     return
 end
 
-@inline function launch_with_bc(backend, launcher, groupsize, kernel, bc, args...)
+@inline function launch_with_bc(arch, grid, launcher, offset, kernel, bc, args...)
+    backend   = Architectures.get_backend(arch)
+    groupsize = heuristic_groupsize(backend, Val(ndims(launcher)))
     inner_fun = kernel(backend, groupsize, inner_worksize(launcher))
-    inner_fun(args..., offset + inner_offset(launcher))
+    inner_fun(args..., offset + Offset(inner_offset(launcher)...))
 
-    if isnothing(outer_width(l))
+    if isnothing(outer_width(launcher))
         bc!(arch, grid, bc)
     else
-        ntuple(Val(ndims(grid))) do D
+        N = ndims(grid)
+        ntuple(Val(N)) do J
             Base.@_inline_meta
+            D = N - J + 1
             outer_fun = kernel(backend, groupsize, outer_worksize(launcher, Dim(D)))
             ntuple(Val(2)) do S
                 put!(launcher.workers[D][S]) do
-                    outer_fun(args..., offset + outer_offset(launcher, Dim(D), Side(S)))
+                    outer_fun(args..., offset + Offset(outer_offset(launcher, Dim(D), Side(S))...))
                     bc!(Side(S), Dim(D), arch, grid, bc[D][S])
                 end
             end
