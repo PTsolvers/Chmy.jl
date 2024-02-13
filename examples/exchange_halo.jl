781a3e78c259b0bccdddbd7b36b2403d8b0d7f92
Ivan Utkin <iutkin@ethz.ch>
2024-02-13 17:13:10 +0100

    Add weak scaling

--- examples/exchange_halo.jl
+++ examples/exchange_halo.jl
@@ -9,9 +9,13 @@ backend = CPU()
 arch = Arch(backend, MPI.COMM_WORLD, (0, 0))
 topo = topology(arch)
 
-grid = UniformGrid(arch; origin=(0, 0), extent=(1, 1), dims=(12, 12))
+# needed for weak scaling
+dims_l = (12, 12)
+dims_g = dims_l .* dims(topo)
 
-field = Field(backend, grid, Center())
+grid = UniformGrid(arch; origin=(0, 0), extent=(1, 1), dims=dims_g)
+
+field = Field(backend, grid, Center(), Int)
 fill!(parent(field), global_rank(topo))
 
 for _ in 1:10
