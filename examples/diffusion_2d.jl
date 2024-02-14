e892643bbb27ca4577339c8acf01b08cf62e1b7e
Ludovic Raess <ludovic.rass@gmail.com>
2024-02-14 09:52:01 +0100

    Update

--- examples/diffusion_2d.jl
+++ examples/diffusion_2d.jl
@@ -1,12 +1,7 @@
 using Chmy, Chmy.Architectures, Chmy.Grids, Chmy.Fields, Chmy.BoundaryConditions, Chmy.GridOperators, Chmy.KernelLaunch
 using KernelAbstractions
-using CairoMakie
 # using AMDGPU
-# AMDGPU.allowscalar(false)
-
-using Chmy.Distributed
-using MPI
-MPI.Init()
+using CairoMakie
 
 @kernel inbounds = true function compute_q!(q, C, χ, g::StructuredGrid, O)
     I = @index(Global, NTuple)
@@ -21,11 +16,11 @@ end
     C[I...] -= Δt * divg(q, g, I...)
 end
 
-@views function main(backend=CPU(); n=254)
-    arch = Arch(backend, MPI.COMM_WORLD, (0, 0))
-    topo = topology(arch)
+@views function main(backend=CPU())
+    arch = Arch(backend)
     # geometry
-    grid = UniformGrid(arch; origin=(-2, -2), extent=(4, 4), dims=(n, n))
+    grid = UniformGrid(arch; origin=(-1, -1), extent=(2, 2), dims=(1022, 1022))
+
     # physics
     χ = 1.0
     # numerics
@@ -33,41 +28,29 @@ end
     # allocate fields
     C = Field(backend, grid, Center())
     q = VectorField(backend, grid)
-    C_v = KernelAbstractions.zeros(CPU(), Float64, size(interior(C)) .* dims(topo))
     # initial conditions
-    set!(C, grid, (x, y) -> exp(-x^2 - y^2))
+    set!(C, grid, (_, _) -> rand())
     bc!(arch, grid, C => Neumann(); exchange=C)
     launch = Launcher(arch, grid)
+    @show typeof(launch)
     # visualisation
     fig = Figure(; size=(400, 320))
     ax  = Axis(fig[1, 1]; aspect=DataAspect(), xlabel="x", ylabel="y", title="it = 0")
     plt = heatmap!(ax, centers(grid)..., interior(C) |> Array; colormap=:turbo)
     Colorbar(fig[1, 2], plt)
+    display(fig)
     # action
     nt = 100
     for it in 1:nt
-        launch(arch, grid, compute_q! => (q, C, χ, grid))
-        launch(arch, grid, update_C! => (C, q, Δt, grid); bc=batch(grid, C => Neumann(); exchange=C))
+        @time launch(arch, grid, compute_q! => (q, C, χ, grid))
+        @time launch(arch, grid, update_C! => (C, q, Δt, grid); bc=batch(grid, C => Neumann(); exchange=C))
     end
     KernelAbstractions.synchronize(backend)
-    # local postprocess
     plt[3] = interior(C) |> Array
     ax.title = "it = $nt"
-    # display(fig)
-    save("out$(global_rank(topo)).png", fig)
-    # global postprocess
-    gather!(arch, C_v, C)
-    if global_rank(topo) == 0
-        fig = Figure(; size=(400, 320))
-        ax  = Axis(fig[1, 1]; aspect=DataAspect(), xlabel="x", ylabel="y", title="it = 0")
-        plt = heatmap!(ax, C_v; colormap=:turbo) # how to get the global grid for axes?
-        Colorbar(fig[1, 2], plt)
-        save("out_gather.png", fig)
-    end
+    display(fig)
     return
 end
 
-# main(ROCBackend(); n=1022)
-main(; n=1022)
-
-MPI.Finalize()
+# main(ROCBackend())
+main()
