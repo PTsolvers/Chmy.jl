6773d20196d02ae17d2e49ea0321886fe3c862c5
Ivan Utkin <iutkin@ethz.ch>
2024-03-01 18:11:34 +0100

    Export symbol

--- src/Grids/Grids.jl
+++ src/Grids/Grids.jl
@@ -6,7 +6,7 @@ export AbstractAxis, UniformAxis, FunctionAxis
 export StructuredGrid, UniformGrid
 
 export nvertices, ncenters, spacing, inv_spacing, Δ, iΔ, coord, coords, center, vertex, centers, vertices
-export origin, extent, bounds
+export origin, extent, bounds, axis
 export direction, axes_names
 export connectivity
 
