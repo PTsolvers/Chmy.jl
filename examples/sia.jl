445b3ff3ea429954c18a3367cfae9c3d2d3d5fc9
Ivan Utkin <iutkin@ethz.ch>
2024-05-03 16:01:13 +0200

    Update example

--- examples/sia.jl
+++ examples/sia.jl
@@ -4,23 +4,12 @@ using KernelAbstractions
 using Printf
 using CairoMakie
 
-@kernel function update_H!(H, q, M, Δt, g)
+@kernel inbounds = true function update_H!(H, q, M, Δt, g)
     I = @index(Global, Cartesian)
     H[I] = max(H[I] + Δt * (-divg(q, g, I) + M[I]), 0.0)
 end
 
-@kernel function update_∇S!(∇S, S, g)
-    I = @index(Global, Cartesian)
-    ∇S.x[I] = ∂x(S, g, I)
-    ∇S.y[I] = ∂y(S, g, I)
-end
-
-@kernel function update_D!(D, H, ∇S, Aₛ, n, g)
-    I = @index(Global, Cartesian)
-    D[I] = (H[I]^(n + 2) + Aₛ * H[I]^n) * vmag(∇S, g, I)^(n - 1)
-end
-
-@kernel function update_q!(q, D, S, g)
+@kernel inbounds = true function update_q!(q, D, S, g)
     I = @index(Global, Cartesian)
     q.x[I] = -lerp(D, location(q.x), g, I) * ∂x(S, g, I)
     q.y[I] = -lerp(D, location(q.y), g, I) * ∂y(S, g, I)
@@ -30,68 +19,80 @@ function main()
     backend = CPU()
     arch    = Arch(backend)
 
-    grid = UniformGrid(arch; origin=(-1, -1), extent=(2, 2), dims=(100, 100))
+    grid = UniformGrid(arch; origin=(-1, -1), extent=(2, 2), dims=(256, 256))
 
-    Aₛ = 1.0
+    Aₛ = 0.5
     n  = 3
 
-    H  = Field(arch, grid, Center())
-    D  = Field(arch, grid, Center())
-    ∇S = VectorField(arch, grid)
-    q  = VectorField(arch, grid)
+    H = Field(arch, grid, Center())
+    q = VectorField(arch, grid)
 
     B = FunctionField(grid, Center()) do x, y
-        bump1  = 0.1 * exp(-((x - 0.5) / 0.3)^2 - ((y - 0.0) / 0.5)^2)
-        bump2  = 0.1 * exp(-((x + 0.5) / 0.3)^2 - ((y - 0.0) / 0.5)^2)
-        bump3  = 0.5 * exp(-(x / 0.5)^2 - ((y - 0.5) / 0.5)^2)
-        linear = 0.08 * y
-        return 0.1 + bump1 + bump2 + bump3 + linear
+        @inline
+        bump1 = 0.3 * exp(-((x - 0.5) / 0.3)^2 - ((y - 0.0) / 0.5)^2)
+        bump2 = 0.3 * exp(-((x + 0.5) / 0.3)^2 - ((y - 0.0) / 0.5)^2)
+        bump3 = 1.0 * exp(-(x / 0.5)^2 - ((y - 0.5) / 0.5)^2)
+        return bump1 + bump2 + bump3
     end
 
-    S = FunctionField(grid, Center(); discrete=true, parameters=(B, H)) do grid, loc, ix, iy, B, H
+    S = FunctionField(grid, Center(); discrete=true, parameters=(B, H)) do g, l, ix, iy, B, H
+        @inline
         return B[ix, iy] + H[ix, iy]
     end
 
+    ∇S = (x=FunctionField((g, l, ix, iy, S) -> ∂x(S, g, ix, iy), grid, (Vertex(), Center()); discrete=true, parameters=(S,)),
+          y=FunctionField((g, l, ix, iy, S) -> ∂y(S, g, ix, iy), grid, (Center(), Vertex()); discrete=true, parameters=(S,)))
+
+    D = FunctionField(grid, Center(); discrete=true, parameters=(H, ∇S, Aₛ, n)) do g, l, ix, iy, H, ∇S, Aₛ, n
+        @inline
+        return (H[ix, iy]^(n + 2) + Aₛ * H[ix, iy]^n) * vmag(∇S, g, ix, iy)^(n - 1)
+    end
+
     M = FunctionField(grid, Center(); discrete=true, parameters=(S,)) do grid, loc, ix, iy, S
-        return min(30.0 * (S[ix, iy] - 0.2), 0.01)
+        @inline
+        return min(0.5 * (S[ix, iy] - 0.3), 0.25)
     end
 
     fig = Figure()
     ax  = Axis(fig[1, 1][1, 1]; aspect=DataAspect(), xlabel="x", ylabel="y")
-    hm  = heatmap!(ax, centers(grid)..., Array(S))
+    hm  = heatmap!(ax, centers(grid)..., Array(S); colormap=:turbo)
     Colorbar(fig[1, 1][1, 2], hm)
     display(fig)
 
-    wg = 256
-    ws = size(grid, Center()) .+ 2
+    wg = (32, 8)
+    ws = size(grid, Center())
 
-    ttot = 50.0
-    tcur = 0.0
-    it   = 1
+    ttot   = 1.0
+    tcur   = 0.0
+    it     = 1
+    Δt_out = 0.1
+    tout   = 0.0
 
-    while tcur < ttot
-        update_∇S!(backend, wg, ws)(∇S, S, grid)
-        update_D!(backend, wg, ws)(D, H, ∇S, Aₛ, n, grid)
+    while tcur <= ttot
         update_q!(backend, wg, ws)(q, D, S, grid)
         bc!(arch, grid, q.x => (x = Dirichlet()), q.y => (y = Dirichlet()))
 
+        # compute time step
         Δt_diff = minimum(spacing(grid))^2 / maximum(interior(D)) / 4.1
-        Δt_mb   = 0.1
-        Δt      = inv(inv(Δt_diff) + inv(Δt_mb))
+        Δt_mb   = 0.5
+        Δt      = min(Δt_diff, Δt_mb, Δt_out - tout)
 
         update_H!(backend, wg, ws)(H, q, M, Δt, grid)
         bc!(arch, grid, H => Neumann())
 
-        if it % 10 == 0
+        if tout >= Δt_out
             @printf("t = %1.3e/%1.3e\n", tcur, ttot)
 
-            hm[3] = Array(S)
-            yield()
-            # display(fig)
+            hm[3] = Array(H)
+            # yield()
+            display(fig)
+
+            tout = 0.0
         end
 
         it   += 1
         tcur += Δt
+        tout += Δt
     end
 
     return
