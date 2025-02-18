331a9d700d96f02b0cf26535592a6e5af78f236b
Ludovic Raess <ludovic.rass@gmail.com>
2025-02-18 15:35:33 +0100

    Transition to vitepress

--- docs/make.jl
+++ docs/make.jl
@@ -1,40 +1,42 @@
 using Documenter
-using Chmy
+using DocumenterVitepress
 
-push!(LOAD_PATH,"../src/")
+using Chmy
 
 makedocs(
     sitename = "Chmy.jl",
     authors="Ivan Utkin, Ludovic Räss and contributors",
-    format = Documenter.HTML(
-        prettyurls=get(ENV, "CI", nothing) == "true", # easier local build
-        ansicolor=true,
-        assets = ["assets/favicon.ico"],
-        ),
+    format = DocumenterVitepress.MarkdownVitepress(
+        repo = "github.com/PTsolvers/Chmy.jl",
+        devbranch = "main",
+        devurl = "dev",
+    ),
     modules = [Chmy],
     warnonly = [:missing_docs],
-    pages = Any[
+    pages = [
         "Home" => "index.md",
         "Getting Started with Chmy.jl" => "getting_started.md",
         "Using Chmy.jl with MPI" => "using_chmy_with_mpi.md",
-        "Concepts" => Any["concepts/architectures.md",
-                        "concepts/grids.md",
-                        "concepts/fields.md",
-                        "concepts/bc.md",
-                        "concepts/grid_operators.md",
-                        "concepts/kernels.md",
-                        "concepts/distributed.md"
+        "Concepts" => [
+            "concepts/architectures.md",
+            "concepts/grids.md",
+            "concepts/fields.md",
+            "concepts/bc.md",
+            "concepts/grid_operators.md",
+            "concepts/kernels.md",
+            "concepts/distributed.md"
         ],
-        "Examples" => Any["examples/overview.md"
+        "Examples" => ["examples/overview.md"],
+        "Library" => ["lib/modules.md"],
+        "Developer documentation" => [
+            "developer_documentation/running_tests.md",
+            "developer_documentation/workers.md"
         ],
-        "Library" => Any["lib/modules.md"],
-        "Developer documentation" => Any["developer_documentation/running_tests.md",
-                                         "developer_documentation/workers.md"],
     ]
 )
 
 deploydocs(
-    repo = "github.com/PTsolvers/Chmy.jl.git",
+    repo = "github.com/PTsolvers/Chmy.jl",
     devbranch = "main",
     push_preview = true
 )
