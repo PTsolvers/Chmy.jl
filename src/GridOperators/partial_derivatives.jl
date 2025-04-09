7ce4c4bb586b96b614bf853a9506448e741a438c
Ludovic Raess <ludovic.rass@gmail.com>
2025-04-09 15:16:46 +0200

    Fixup

--- src/GridOperators/partial_derivatives.jl
+++ src/GridOperators/partial_derivatives.jl
@@ -3,6 +3,7 @@
 
 # partial derivative
 @add_cartesian ∂(f, loc, from, grid, dim, I::Vararg{Integer,N}) where {N} = δ(f, loc, from, dim, I...) * iΔ(grid, loc, dim, I...)
-@add_cartesian ∂²(f, loc, from, grid, dim, I::Vararg{Integer,N}) where {N} = ((right(f, loc, from, dim, I...) - f[I...]) * iΔ(grid, loc, dim, I...) -
-                                                                              (f[I...] - left(f, loc, from, dim, I...) ) * iΔ(grid, loc, dim, I...)) *
-                                                                             iΔ(grid, loc, dim, I...)
+
+@add_cartesian ∂²(f, loc, from, grid, dim::Dim{D}, I::Vararg{Integer,N}) where {N,D} = (∂(f, grid, dim, ir(flipped(loc, dim)[D], from[D], dim, I...)...) -
+                                                                                        ∂(f, grid, dim, il(flipped(loc, dim)[D], from[D], dim, I...)...)) *
+                                                                                       iΔ(grid, loc, dim, I...)
