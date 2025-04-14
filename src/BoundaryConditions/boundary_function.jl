6909ad0c0644d26afd4c2b932db34c9c5c7b58c5
Ivan Utkin <iutkin@ethz.ch>
2025-04-14 13:35:56 +0200

    Modify usage example

--- src/BoundaryConditions/boundary_function.jl
+++ src/BoundaryConditions/boundary_function.jl
@@ -44,27 +44,29 @@ end
 end
 
 """
-    BoundaryFunction(fun::Function; discrete=false, parameters=nothing, reduce_dims=true)
+    BoundaryFunction(fun; discrete=false, parameters=nothing, reduce_dims=true)
 
-Creates a boundary function that can either be continuous or discrete, depending on the `discrete` keyword argument.
+Creates a "boundary function" object that can be use to define boundary conditions on fields.
 
 ## Arguments
-- `fun::Function`: The function defining the boundary condition.
-- `discrete::Bool=false`: If `true`, the boundary function is discrete and has the signature `f(grid, loc, dim, inds...)`.
-- `parameters`: Optional parameters to be passed to the boundary function.
-- `reduce_dims::Bool=true`: If `true`, the boundary function reduces the number of dimensions it operates on. If `false`, the function accepts the same number of coordinates as the number of indices.
+- `fun`: The function defining the boundary condition.
+- `discrete=false`: If `true`, the boundary function is discrete and has the signature `f(grid, loc, dim, inds...)`.
+- `parameters=nothing`: Optional parameters to be passed to the boundary function.
+- `reduce_dims=true`: If `true`, the boundary function reduces the number of dimensions it operates on. If `false`, the function accepts the same number of coordinates as the number of indices.
 
 ## Usage
-The example below shows how to use the boundary function to initialise a pure shear configuration in 2D.
+The example below shows how to use the boundary function to initialise a parabolic profile at the boundary of a 2D grid.
+
 ```julia
-ebg = 1.0
-psh_x(x) = -x * ebg
-psh_y(y) =  y * ebg
-x_bc = BoundaryFunction(psh_x; reduce_dims=true)
-y_bc = BoundaryFunction(psh_y; reduce_dims=true)
-bc_V = (V.x => (x=Dirichlet(x_bc), y=Neumann()),
-        V.y => (x=Neumann(), y=Dirichlet(y_bc)))
-bc!(arch, grid, bc_V...)
+using Chmy
+
+arch = Arch(CPU())
+grid = UniformGrid(arch; origin=(0, 0), extent=(2, 2), dims=(10, 10))
+
+f = Field(arch, grid, Center())
+xbc = BoundaryFunction((x, ly) -> x * (ly - x); parameters=(2.0, ))
+
+bc!(arch, grid, f => (x = Dirichlet(xbc), y = Neumann()))
 ```
 """
 function BoundaryFunction(fun::Function; discrete=false, parameters=nothing, reduce_dims=true)
