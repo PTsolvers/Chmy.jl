5b27541ab033a5d1720fc1a7c35a79abf3d9c486
Ivan Utkin <iutkin@ethz.ch>
2025-04-14 13:36:09 +0200

    Remove type signature

--- src/Fields/function_field.jl
+++ src/Fields/function_field.jl
@@ -29,7 +29,7 @@ Create a `FunctionField` on the given `grid` using the specified function `func`
 - `func::F`: The function used to generate the field values.
 - `grid::StructuredGrid{N}`: The structured grid defining the computational domain.
 - `loc`: The nodal location on the grid grid where the function field is defined on.
-- `discrete::Bool=false`: A flag indicating whether the field should be discrete. Defaults to `false`.
+- `discrete=false`: A flag indicating whether the field should be discrete. Defaults to `false`.
 - `parameters=nothing`: Additional parameters to be used by the function. Defaults to `nothing`.
 """
 function FunctionField(func::F, grid::StructuredGrid{N}, loc; discrete=false, parameters=nothing) where {F,N}
