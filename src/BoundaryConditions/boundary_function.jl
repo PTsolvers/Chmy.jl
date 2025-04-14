856ebb50ede5afb0f472cde1676fa3ea843a7abb
Ludovic Räss <61313342+luraess@users.noreply.github.com>
2025-04-14 14:48:15 +0200

    Document BoundaryFunction (#83)
    Co-authored-by: Ivan Utkin <iutkin@ethz.ch>

--- src/BoundaryConditions/boundary_function.jl
+++ src/BoundaryConditions/boundary_function.jl
@@ -1,6 +1,6 @@
 """
     abstract type BoundaryFunction{F}
-    
+
 Abstract type for boundary condition functions with function type `F`.
 """
 abstract type BoundaryFunction{F} end
@@ -43,9 +43,32 @@ end
     bc.value(grid, loc, dim, I...)
 end
 
-# Create a continuous or discrete boundary function
-# if discrete = true, the function has signature f(grid, loc, dim, inds...)
-# if reduce_dims = false, the boundary condition function accepts the same number of coordinates as the number of indices
+"""
+    BoundaryFunction(fun; discrete=false, parameters=nothing, reduce_dims=true)
+
+Creates a "boundary function" object that can be use to define boundary conditions on fields.
+
+## Arguments
+- `fun`: The function defining the boundary condition.
+- `discrete=false`: If `true`, the boundary function is discrete and has the signature `f(grid, loc, dim, inds...)`.
+- `parameters=nothing`: Optional parameters to be passed to the boundary function.
+- `reduce_dims=true`: If `true`, the boundary function reduces the number of dimensions it operates on. If `false`, the function accepts the same number of coordinates as the number of indices.
+
+## Usage
+The example below shows how to use the boundary function to initialise a parabolic profile at the boundary of a 2D grid.
+
+```julia
+using Chmy
+
+arch = Arch(CPU())
+grid = UniformGrid(arch; origin=(0, 0), extent=(2, 2), dims=(10, 10))
+
+f = Field(arch, grid, Center())
+xbc = BoundaryFunction((x, ly) -> x * (ly - x); parameters=(2.0, ))
+
+bc!(arch, grid, f => (x = Dirichlet(xbc), y = Neumann()))
+```
+"""
 function BoundaryFunction(fun::Function; discrete=false, parameters=nothing, reduce_dims=true)
     RF = reduce_dims ? ReducedDimensions : FullDimensions
     discrete ? DiscreteBoundaryFunction{RF}(fun, parameters) : ContinuousBoundaryFunction{RF}(fun, parameters)
