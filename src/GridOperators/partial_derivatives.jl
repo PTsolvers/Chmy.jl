a07c1c7942b19b62b1e020d56ad6498b4e3fb3df
Ivan Utkin <iutkin@ethz.ch>
2025-04-14 15:33:05 +0200

    Refactor

--- src/GridOperators/partial_derivatives.jl
+++ src/GridOperators/partial_derivatives.jl
@@ -11,9 +11,10 @@
 end
 
 @add_cartesian function ∂k∂(f, k, loc, from, grid, dim::Dim{D}, I::Vararg{Integer,N}) where {N,D}
-    Ir = ir(flipped(loc, dim)[D], from[D], dim, I...)
-    Il = il(flipped(loc, dim)[D], from[D], dim, I...)
-    return (lerp(k, flipped(loc, dim), grid, Ir...) * ∂(f, grid, dim, Ir...) -
-            lerp(k, flipped(loc, dim), grid, Il...) * ∂(f, grid, dim, Il...)) *
+    floc = flipped(loc, dim)
+    Ir = ir(floc[D], from[D], dim, I...)
+    Il = il(floc[D], from[D], dim, I...)
+    return (lerp(k, floc, grid, Ir...) * ∂(f, grid, dim, Ir...) -
+            lerp(k, floc, grid, Il...) * ∂(f, grid, dim, Il...)) *
            iΔ(grid, loc, dim, I...)
 end
