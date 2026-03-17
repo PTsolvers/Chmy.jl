c6df1812684b3010eaa2dc6271d6973d31a9a6cd
Ivan Utkin <iutkin@ethz.ch>
2026-03-17 10:47:51 +0100

    Refactor pretty-printing

--- src/show.jl
+++ src/show.jl
@@ -49,40 +49,15 @@ function show_static(io, expr::SExpr, prec::Int)
         op = operation(expr)
         args = arguments(expr)
         opname = variablename(op)
-        op_prec = Base.operator_precedence(opname)
-        if Meta.isoperator(opname)
-            if length(args) == 1
-                parens = needs_parens(expr, prec)
-                parens && print(io, '(')
-                print(io, opname)
-                show_static(io, args[1], op_prec)
-                parens && print(io, ')')
-            else
-                if opname == :* &&
-                   length(args) == 2 &&
-                   args[1] isa SUniform &&
-                   !(args[2] isa SUniform) &&
-                   !isexpr(args[2]) &&
-                   variablename(args[1]) isa Union{AbstractFloat,Integer}
-                    sep = ""
-                else
-                    sep = " $opname "
-                end
-                parens = needs_parens(expr, prec)
-                parens && print(io, '(')
-                show_list(io, args, sep, op_prec, opname)
-                parens && print(io, ')')
-            end
+        if op === SRef(:broadcasted)
+            show_broadcast(io, args, prec)
+        elseif Meta.isoperator(opname)
+            show_operator(io, opname, args, prec)
         elseif opname == :adjoint
-            if isexpr(args[1]) && iscall(args[1])
-                print(io, '(')
-                show_static(io, args[1], 0)
-                print(io, ')')
-            else
-                show_static(io, args[1], 0)
-            end
+            show_call_arg(io, only(args))
             print(io, ''')
         else
+            op_prec = Base.operator_precedence(opname)
             print(io, opname)
             print(io, '(')
             show_list(io, args, ", ", op_prec)
@@ -95,26 +70,12 @@ function show_static(io, expr::SExpr, prec::Int)
         show_list(io, indices(expr), ", ", 0)
         print(io, ']')
     elseif isind(expr)
-        arg = argument(expr)
-        if !isexpr(arg) || !iscall(arg)
-            show_static(io, arg, 0)
-        else
-            print(io, '(')
-            show_static(io, arg, 0)
-            print(io, ')')
-        end
+        show_call_arg(io, argument(expr))
         print(io, '[')
         show_list(io, indices(expr), ", ", 0)
         print(io, ']')
     elseif isloc(expr)
-        arg = argument(expr)
-        if !isexpr(arg) || !iscall(arg)
-            show_static(io, arg, 0)
-        else
-            print(io, '(')
-            show_static(io, arg, 0)
-            print(io, ')')
-        end
+        show_call_arg(io, argument(expr))
         for s in location(expr)
             print(io, variablename(s))
         end
@@ -122,32 +83,79 @@ function show_static(io, expr::SExpr, prec::Int)
     return
 end
 
-function show_list(io, items, sep, prec, parent_opname=nothing)
-    for (i, item) in enumerate(items)
-        i > 1 && print(io, sep)
-        show_static(io, item, child_precedence(parent_opname, item, prec))
+function show_broadcast(io, args, prec)
+    op = first(args)
+    bargs = Base.tail(args)
+    opname = variablename(op)
+    if Meta.isoperator(opname)
+        show_operator(io, Symbol('.', opname), bargs, prec; parent_opname=opname)
+    else
+        op_prec = Base.operator_precedence(opname)
+        print(io, opname)
+        print(io, ".(")
+        show_list(io, bargs, ", ", op_prec)
+        print(io, ')')
     end
     return
 end
 
-child_precedence(::Nothing, item, prec) = prec
+function show_operator(io, display_opname::Symbol, args, prec; parent_opname::Symbol=display_opname)
+    op_prec = Base.operator_precedence(parent_opname)
+    parens = needs_parens(op_prec, prec)
+    parens && print(io, '(')
+    if length(args) == 1
+        print(io, display_opname)
+        show_static(io, only(args), op_prec)
+    else
+        show_list(io, args, operator_separator(display_opname, parent_opname, args), op_prec, parent_opname)
+    end
+    parens && print(io, ')')
+    return
+end
 
-function child_precedence(parent_opname::Symbol, item, prec)
+function operator_separator(display_opname::Symbol, parent_opname::Symbol, args)
+    if parent_opname == :* &&
+       length(args) == 2 &&
+       args[1] isa SUniform &&
+       !(args[2] isa SUniform) &&
+       !isexpr(args[2]) &&
+       variablename(args[1]) isa Union{AbstractFloat,Integer}
+        return ""
+    end
+    return " $display_opname "
+end
+
+function show_call_arg(io, arg)
+    if isexpr(arg) && iscall(arg)
+        print(io, '(')
+        show_static(io, arg, 0)
+        print(io, ')')
+    else
+        show_static(io, arg, 0)
+    end
+    return
+end
+
+function show_list(io, items, sep, prec, parent_opname=nothing)
+    for (i, item) in enumerate(items)
+        i > 1 && print(io, sep)
+        show_static(io, item, child_precedence(parent_opname, item, prec, i))
+    end
+    return
+end
+
+child_precedence(::Nothing, _, prec, _) = prec
+
+function child_precedence(parent_opname::Symbol, item, prec, i)
     if parent_opname in (:+, :-) && isunaryminus(item)
+        return prec - 1
+    elseif i == 1 && parent_opname in (:+, :-, :*, :/, ://, :÷, :⋅, :×, :⊡, :⊗, :&, :|, :xor)
         return prec - 1
     end
     return prec
 end
 
-function needs_parens(expr, prec)
-    iscall(expr) || return false
-    op = operation(expr)
-    op_prec = Base.operator_precedence(variablename(op))
-    if op_prec == 0 || prec == 0
-        return false
-    end
-    return op_prec <= prec
-end
+needs_parens(op_prec::Int, prec::Int) = op_prec != 0 && prec != 0 && op_prec <= prec
 
 function Base.show(io::IO, ::MIME"text/plain", v::Vec{N}) where {N}
     print(io, "$N-element Vec:")
--- test/test_show.jl
+++ test/test_show.jl
@@ -4,4 +4,8 @@
 
     @test sprint(show, -a + b) == "-a + b"
     @test sprint(show, (-a)^b) == "(-a) ^ b"
+    @test sprint(show, a + b - a / b) == "a + b - a / b"
+    @test sprint(show, Chmy.makeop(:-, a, Chmy.makeop(:+, b, a^b))) == "a - (b + a ^ b)"
+    @test sprint(show, a .+ b) == "a .+ b"
+    @test sprint(show, sin.(a)) == "sin.(a)"
 end
