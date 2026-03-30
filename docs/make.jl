2c63e5bbdee32d011d20be85ce3344076d749df1
Ivan Utkin <iutkin@ethz.ch>
2026-03-30 16:59:02 +0200

    Update docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,23 +3,12 @@ using DocumenterVitepress
 
 using Chmy
 
-makedocs(
-    sitename = "Chmy.jl",
-    authors="Ivan Utkin and contributors",
-    format = DocumenterVitepress.MarkdownVitepress(
-        repo = "github.com/PTsolvers/Chmy.jl",
-        devbranch = "main",
-        devurl = "dev",
-    ),
-    modules = [Chmy],
-    warnonly = [:missing_docs],
-    pages = [
-        "Home" => "index.md",
-    ]
-)
+makedocs(;
+         sitename = "Chmy.jl",
+         authors  = "Ivan Utkin and contributors",
+         format   = DocumenterVitepress.MarkdownVitepress(; repo="github.com/PTsolvers/Chmy.jl", devbranch="main", devurl="dev"),
+         modules  = [Chmy],
+         warnonly = [:missing_docs],
+         pages    = ["Home" => "index.md"])
 
-deploydocs(
-    repo = "github.com/PTsolvers/Chmy.jl",
-    devbranch = "main",
-    push_preview = true
-)
+DocumenterVitepress.deploydocs(; repo="github.com/PTsolvers/Chmy.jl", devbranch="main", push_preview=true)
