8b1c22c48fdfa42a59ade442a4910146667fddd4
Ivan Utkin <iutkin@ethz.ch>
2026-09-09 23:32:27 +0200

    Re-implement operators

--- src/operators.jl
+++ src/operators.jl
@@ -1,20 +1,9 @@
-struct Fun{F} <: Operator
-    f::F
-    function Fun(f)
-        if Base.issingletontype(typeof(f))
-            return new{typeof(f)}(f)
-        else
-            throw(ArgumentError("function must be a singleton type"))
-        end
-    end
-end
-
-@inline function makecall(op::Fun, args::NTuple{N,DTerm}) where {N}
+@inline function makecall(op::Operator, args::NTuple{N,DTerm}) where {N}
     checkranks(op, args)
     makecall_unchecked(op, args)
 end
-makecall(op::Fun, args::Vararg{DTerm,N}) where {N} = makecall(op, args)
-makecall_unchecked(op::Fun, args::NTuple{N,DTerm}) where {N} = DExpr(Call(op), args)
+makecall(op::Operator, args::Vararg{DTerm,N}) where {N} = makecall(op, args)
+makecall_unchecked(op::Operator, args::NTuple{N,DTerm}) where {N} = DExpr(Call(op), args)
 
 function makecomp(arg::DTerm, comp::NTuple{N,Int}) where {N}
     tensorrank(arg) == length(comp) || throw(ArgumentError("number of components must be equal to the rank of the tensor"))
@@ -37,6 +26,20 @@ end
 makeinds(arg::DTerm, inds::Vararg{DTerm,N}) where {N} = makeinds(arg, inds)
 makeinds_unchecked(arg::DTerm, inds::NTuple{N,DTerm}) where {N} = DExpr(Inds(), (arg, inds...))
 
+# make operators callable
+(op::Operator)(args::DTerm...) = makecall(op, args)
+
+struct Fun{F} <: Operator
+    f::F
+    function Fun(f)
+        if Base.issingletontype(typeof(f))
+            return new{typeof(f)}(f)
+        else
+            throw(ArgumentError("function must be a singleton type"))
+        end
+    end
+end
+
 function ⊡ end
 function ⊗ end
 
@@ -47,6 +50,9 @@ function adj end
 
 function gram end
 function cogram end
+
+# unless specified otherwise, we assume all operators are scalar
+tensorrank(op::Operator, args) = 0
 
 tensorrank(op::Fun, args) = tensorrank(op.f, args)
 
@@ -73,14 +79,17 @@ tensorrank(::typeof(gram), args)    = 2
 tensorrank(::typeof(cogram), args)  = 2
 tensorrank(::typeof(diag), args)    = 1
 
-@inline checkranks(op::Fun, args::NTuple{N,DTerm}) where {N} = checkranks(op.f, args)
-
-# fallback implementation for all functions, accepting only scalar arguments
-function checkranks(f::Function, args::NTuple{N,DTerm}) where {N}
+@inline function checkscalar(op, args)
     if any(x -> tensorrank(x) > 0, args)
-        throw(ArgumentError(LazyString("all arguments to '", f, "' must be scalars")))
+        throw(ArgumentError(LazyString("all arguments to '", op, "' must be scalars")))
     end
 end
+
+checkranks(op::Fun, args::NTuple{N,DTerm}) where {N} = checkranks(op.f, args)
+
+# fallback implementation for all functions and operators, accepting only scalar arguments
+checkranks(op::Operator, args::NTuple{N,DTerm}) where {N} = checkscalar(op, args)
+checkranks(f::Function, args::NTuple{N,DTerm}) where {N} = checkscalar(f, args)
 
 for op in (:+, :-)
     @eval function checkranks(::typeof($op), args::NTuple{N,DTerm}) where {N}
@@ -116,7 +125,7 @@ function checkranks(::typeof(×), args::NTuple{N,DTerm}) where {N}
     end
 end
 function checkranks(::typeof(⋅), args::NTuple{N,DTerm}) where {N}
-    if tensorrank(args[1]) + tensorrank(args[2]) < 2
+    if tensorrank(args[1]) < 1 || tensorrank(args[2]) < 1
         throw(ArgumentError("dot product '⋅' requires both arguments to have rank at least 1"))
     end
 end
@@ -134,6 +143,7 @@ end
 
 # shortcut for unary plus
 Base.:+(x::DTerm) = x
+Base.:*(x::DTerm) = x
 
 # multiary operators
 for op in (:+, :*, :max, :min)
@@ -159,9 +169,16 @@ function Base.adjoint(t::DTerm)
 end
 
 """
+    ⋅(a::DTerm, b::DTerm)
+
+A single contraction operator, which contracts the last index of `a` with the first index of `b`.
+"""
+⋅(a::DTerm, b::DTerm) = makecall(Fun(⋅), a, b)
+
+"""
     ⊡(a, b)
 
-The double contraction operator, which contracts the first two indices of `a` with the first two indices of `b`.
+The double contraction operator, which contracts the last two indices of `a` with the first two indices of `b`.
 """
 ⊡(a::DTerm, b::DTerm) = makecall(Fun(⊡), a, b)
 
@@ -182,13 +199,7 @@ for op in (:det, :tr, :diag)
     end
 end
 
-function Base.inv(t::DTerm)
-    if iscall(t) && operation(t) === Fun(inv)
-        return only(arguments(t))
-    end
-    return makecall(Fun(inv), t)
-end
-
+Base.inv(t::DTerm) = makecall(Fun(inv), t)
 ×(a::DTerm, b::DTerm) = makecall(Fun(×), a, b)
 
 """
@@ -231,11 +242,7 @@ end
 # scalar unary operations
 isunaryminus(expr::DTerm) = isunary(expr) && operation(expr) === (-)
 
-Base.:-(arg::DTerm) = makecall(Fun(-), arg)
-
-Base.:-(::SLiteral{S}) where {S} = SLiteral{-S}()
-Base.:-(arg::SLiteral{0}) = arg
-Base.:-(arg::SZeroTensor) = arg
+Base.:-(arg::DTerm) = makecall_unchecked(Fun(-), (arg,))
 
 for op in (:sqrt, :abs,
            :sin, :cos, :tan,
@@ -264,201 +271,7 @@ end
 Base.ifelse(cond::DTerm, x::DTerm, y::DTerm) = makecall(Fun(ifelse), cond, x, y)
 Base.ifelse(cond::DTerm, x::Number, y::DTerm) = ifelse(cond, Literal(x), y)
 Base.ifelse(cond::DTerm, x::DTerm, y::Number) = ifelse(cond, x, Literal(y))
-Base.ifelse(cond::DTerm, x::Number, y::Number) = ifelse(cond, Literal(x), Literal(y))
-
-# tensor operations
-
-# @generated function ⊡(t1::Tensor{D,2}, t2::Tensor{D,2}) where {D}
-#     ex_p = Tuple(:(t1[$i, $j] * t2[$i, $j]) for i in 1:D, j in 1:D)
-#     ex_c = Expr(:call, :+, ex_p...)
-#     quote
-#         @inline
-#         return $ex_c
-#     end
-# end
-
-# ⋅(v1::Vec{D}, v2::Vec{D}) where {D} = +(ntuple(i -> v1[i] * v2[i], Val(D))...)
-# @generated function ⋅(t::Tensor{D,2}, v::Vec{D}) where {D}
-#     vc(i) = Expr(:call, :+, Tuple(:(t[$i, $j] * v[$j]) for j in 1:D)...)
-#     ex = Expr(:call, :(Vec{$D}), Tuple(vc(i) for i in 1:D)...)
-#     quote
-#         @inline
-#         return $ex
-#     end
-# end
-# @generated function ⋅(t1::Tensor{D,2}, t2::Tensor{D,2}) where {D}
-#     vc(i, j) = Expr(:call, :+, Tuple(:(t1[$i, $k] * t2[$k, $j]) for k in 1:D)...)
-#     ex = Expr(:call, :(Tensor{$D,2}), Tuple(vc(i, j) for i in 1:D, j in 1:D)...)
-#     quote
-#         @inline
-#         return $ex
-#     end
-# end
-
-# function ×(v1::Vec{3}, v2::Vec{3})
-#     return Vec{3}(v1[2] * v2[3] - v1[3] * v2[2],
-#                   v1[3] * v2[1] - v1[1] * v2[3],
-#                   v1[1] * v2[2] - v1[2] * v2[1])
-# end
-
-# @generated function ⊗(v1::Vec{D}, v2::Vec{D}) where {D}
-#     ex = Expr(:call, :(Tensor{$D,2}))
-#     for j in 1:D, i in 1:D
-#         push!(ex.args, :(v1[$i] * v2[$j]))
-#     end
-#     quote
-#         @inline
-#         return $ex
-#     end
-# end
-
-# tr(t::Tensor{D,2}) where {D} = +(ntuple(i -> t[i, i], Val(D))...)
-
-# Base.transpose(t::SymTensor{2}) = t
-# Base.transpose(t::AltTensor{2}) = -t
-# Base.transpose(t::DiagTensor{2}) = t
-# @generated function Base.transpose(t::Tensor{D,2}) where {D}
-#     ex = Expr(:call, :(Tensor{$D,2}), Tuple(:(t[$j, $i]) for i in 1:D, j in 1:D)...)
-#     quote
-#         @inline
-#         return $ex
-#     end
-# end
-
-# Base.adjoint(S::Tensor{D,2}) where {D} = transpose(S)
-
-# det(t::Tensor{2,2}) = t[1, 1] * t[2, 2] - t[1, 2] * t[2, 1]
-# det(t::SymTensor{2,2}) = t[1, 1] * t[2, 2] - t[1, 2]^2
-# det(t::AltTensor{2,2}) = t[1, 2]^2
-# function det(t::Tensor{3,2})
-#     t[1, 1] * (t[2, 2] * t[3, 3] - t[2, 3] * t[3, 2]) +
-#     t[1, 2] * (t[2, 3] * t[3, 1] - t[2, 1] * t[3, 3]) +
-#     t[1, 3] * (t[2, 1] * t[3, 2] - t[2, 2] * t[3, 1])
-# end
-
-# det(::AltTensor{2,3}) = SLiteral(0)
-
-# diag(t::Tensor{D,2}) where {D} = Vec(ntuple(i -> t[i, i], Val(D))...)
-
-# adj(t::Tensor{2,2}) = Tensor{2,2}(t[2, 2], -t[2, 1], -t[1, 2], t[1, 1])
-# adj(t::SymTensor{2,2}) = SymTensor{2,2}(t[2, 2], -t[1, 2], t[1, 1])
-# adj(t::AltTensor{2,2}) = AltTensor{2,2}(-t[1, 2])
-# function adj(t::Tensor{3,2})
-#     c11 = t[2, 2] * t[3, 3] - t[2, 3] * t[3, 2]
-#     c12 = t[2, 3] * t[3, 1] - t[2, 1] * t[3, 3]
-#     c13 = t[2, 1] * t[3, 2] - t[2, 2] * t[3, 1]
-#     c21 = t[1, 3] * t[3, 2] - t[1, 2] * t[3, 3]
-#     c22 = t[1, 1] * t[3, 3] - t[1, 3] * t[3, 1]
-#     c23 = t[1, 2] * t[3, 1] - t[1, 1] * t[3, 2]
-#     c31 = t[1, 2] * t[2, 3] - t[1, 3] * t[2, 2]
-#     c32 = t[1, 3] * t[2, 1] - t[1, 1] * t[2, 3]
-#     c33 = t[1, 1] * t[2, 2] - t[1, 2] * t[2, 1]
-#     return Tensor{3,2}(c11, c12, c13,
-#                        c21, c22, c23,
-#                        c31, c32, c33)
-# end
-# function adj(t::SymTensor{2,3})
-#     c11 = t[2, 2] * t[3, 3] - t[2, 3]^2
-#     c12 = t[2, 3] * t[1, 3] - t[1, 2] * t[3, 3]
-#     c13 = t[1, 2] * t[2, 3] - t[2, 2] * t[1, 3]
-#     c22 = t[1, 1] * t[3, 3] - t[1, 3]^2
-#     c23 = t[1, 2] * t[1, 3] - t[1, 1] * t[2, 3]
-#     c33 = t[1, 1] * t[2, 2] - t[1, 2]^2
-#     return SymTensor{2,3}(c11,
-#                           c12, c22,
-#                           c13, c23, c33)
-# end
-# function adj(t::AltTensor{2,3})
-#     c11 = t[2, 3]^2
-#     c12 = -t[1, 3] * t[2, 3]
-#     c13 = t[1, 2] * t[2, 3]
-#     c22 = t[1, 3]^2
-#     c23 = -t[1, 2] * t[1, 3]
-#     c33 = t[1, 2]^2
-#     return SymTensor{2,3}(c11,
-#                           c12, c22,
-#                           c13, c23, c33)
-# end
-
-# Base.inv(t::Tensor{D,2}) where {D} = adj(t) / det(t)
-
-# sym(t::SymTensor) = t
-# @generated function sym(t::Tensor{D,2}) where {D}
-#     ex = Expr(:call, :(SymTensor{$D,2}))
-#     foreach_nondecreasing(Val(D), Val(2)) do (i, j)
-#         push!(ex.args, :(1 // 2 * (t[$i, $j] + t[$j, $i])))
-#     end
-#     quote
-#         @inline
-#         return $ex
-#     end
-# end
-
-# @generated function asym(t::Tensor{D,2}) where {D}
-#     ex = Expr(:call, :(AltTensor{$D,2}))
-#     foreach_increasing(Val(D), Val(2)) do (i, j)
-#         push!(ex.args, :(1 // 2 * (t[$i, $j] - t[$j, $i])))
-#     end
-#     quote
-#         @inline
-#         return $ex
-#     end
-# end
-
-# """
-#     gram(t)
-
-# The Gramian of a second-rank tensor, defined as `t' ⋅ t`.
-# """
-# @generated function gram(t::Tensor{D,2}) where {D}
-#     ex = Expr(:call, :(SymTensor{$D,2}))
-#     foreach_nondecreasing(Val(D), Val(2)) do (i, j)
-#         comp = Expr(:call, :+)
-#         for k in 1:D
-#             push!(comp.args, :(t[$i, $k] * t[$j, $k]))
-#         end
-#         push!(ex.args, comp)
-#     end
-#     quote
-#         @inline
-#         return $ex
-#     end
-# end
-
-# """
-#     cogram(t)
-
-# The co-Gramian of a second-rank tensor, defined as `t ⋅ t'`.
-# """
-# @generated function cogram(t::Tensor{D,2}) where {D}
-#     ex = Expr(:call, :(SymTensor{$D,2}))
-#     foreach_nondecreasing(Val(D), Val(2)) do (i, j)
-#         comp = Expr(:call, :+)
-#         for k in 1:D
-#             push!(comp.args, :(t[$k, $i] * t[$k, $j]))
-#         end
-#         push!(ex.args, comp)
-#     end
-#     quote
-#         @inline
-#         return $ex
-#     end
-# end
-
-# # custom broadcasting for tensors
-# @generated function Base.Broadcast.broadcasted(f::F, arg::Tensor{D,R}) where {F,D,R}
-#     ex = Expr(:call, :(Tensor{$D,$R}))
-#     for idx in CartesianIndices(ntuple(_ -> D, Val(R)))
-#         I = Tuple(idx)
-#         push!(ex.args, :(f(arg[$(I...)])))
-#     end
-#     quote
-#         @inline
-#         return $ex
-#     end
-# end
-
-# TODO: perform checks as in a static version
+Base.ifelse(cond::DTerm, x::Number, y::Number) = ifelse(cond, Literal(x), Literal(y))å
 
 # location
 Base.getindex(arg::DTerm, locs::Vararg{Location,N}) where {N} = makelocs(arg, locs...)
