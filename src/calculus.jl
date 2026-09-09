e765c0a80069e8dfa87a704f4d4cb3b181f9ed22
Ivan Utkin <iutkin@ethz.ch>
2026-09-09 23:31:43 +0200

    Re-implement calculus operators

--- src/calculus.jl
+++ src/calculus.jl
@@ -1,68 +1,74 @@
-abstract type DifferentialOperator <: SHead end
+abstract type DifferentialOperator <: Operator end
 
-(op::DifferentialOperator)(args::Vararg{STerm}) = SExpr(op, args...)
+"""
+    Gradient(op)
 
+Gradient differential operator.
+"""
 struct Gradient{Op} <: DifferentialOperator
     op::Op
 end
-Gradient(op::AbstractDerivative) = Gradient(PartialDerivative(op))
 
+"""
+    Divergence(op)
+
+Divergence differential operator.
+"""
 struct Divergence{Op} <: DifferentialOperator
     op::Op
 end
-Divergence(op::AbstractDerivative) = Divergence(PartialDerivative(op))
 
+"""
+    Curl(op)
+
+Curl differential operator.
+"""
 struct Curl{Op} <: DifferentialOperator
     op::Op
 end
-Curl(op::AbstractDerivative) = Curl(PartialDerivative(op))
 
-tensorrank(::Gradient, t)   = tensorrank(t) + 1
-tensorrank(::Divergence, t) = tensorrank(t) - 1
-tensorrank(::Curl, t)       = 1
+tensorrank(::Gradient, args)   = tensorrank(only(args)) + 1
+tensorrank(::Divergence, args) = tensorrank(only(args)) - 1
+tensorrank(::Curl, args)       = 1
 
-Tensor{D}(grad::Gradient, s::STerm) where {D} = Vec(ntuple(i -> grad.op[i](s), D)...)
-@generated function Tensor{D}(grad::Gradient, t::Tensor{D,R}) where {D,R}
-    ex = Expr(:call, :(Tensor{$D,$(R + 1)}))
-    for idx in CartesianIndices(ntuple(_ -> D, Val(R + 1)))
-        I = Tuple(idx)
-        push!(ex.args, :(grad.op[$(I[1])](t[$(I[2:end]...)])))
-    end
-    quote
-        @inline
-        return $ex
-    end
+function checkranks(::Gradient, ::NTuple{N,DTerm}) where {N}
+    N == 1 || throw(ArgumentError("gradient requires exactly one argument"))
+    return
 end
-@generated function Tensor{D}(divg::Divergence, t::Tensor{D,R}) where {D,R}
-    if R == 1
-        ex = Expr(:call, :+)
-        for i in 1:D
-            push!(ex.args, :(divg.op[$i](t[$i])))
-        end
-    else
-        ex = Expr(:call, :(Tensor{$D,$(R - 1)}))
-        for idx in CartesianIndices(ntuple(_ -> D, Val(R - 1)))
-            I = Tuple(idx)
-            comp = Expr(:call, :+)
-            for i in 1:D
-                push!(comp.args, :(divg.op[$i](t[$i, $(I...)])))
-            end
-            push!(ex.args, comp)
-        end
-    end
-    quote
-        @inline
-        return $ex
+function checkranks(::Divergence, args::NTuple{N,DTerm}) where {N}
+    N == 1 || throw(ArgumentError("divergence requires exactly one argument"))
+    tensorrank(only(args)) > 0 || throw(ArgumentError("divergence requires a tensor of positive rank"))
+    return
+end
+function checkranks(::Curl, args::NTuple{N,DTerm}) where {N}
+    N == 1 || throw(ArgumentError("curl requires exactly one argument"))
+    tensorrank(only(args)) == 1 || throw(ArgumentError("curl requires a rank-1 tensor (vector)"))
+    return
+end
+
+function evaluate(grad::Gradient, args::Tuple{DTerm}, dims::Integer)
+    s = only(args)
+    return TensorComponents(Kind.None(), 1, dims) do (i,)
+        grad.op[i](s)
     end
 end
-function Tensor{D}(::Curl, ::Vec{D}) where {D}
-    throw(ArgumentError("curl is only supported for 2D and 3D vector fields, got dimension $D"))
+function evaluate(grad::Gradient, args::Tuple{TensorComponents}, dims::Integer)
+    t = only(args)
+    return TensorComponents(Kind.None(), t.rank + 1, dims) do I
+        grad.op[I[1]](t[Base.tail(I)...])
+    end
 end
-function Tensor{2}(curl::Curl, v::Vec{2})
-    return curl.op[1](v[2]) - curl.op[2](v[1])
+function evaluate(divg::Divergence, args::Tuple{TensorComponents}, dims::Integer)
+    t = only(args)
+    component(I) = component_sum(DTerm[divg.op[i](t[i, I...]) for i in 1:dims])
+    t.rank == 1 && return component(())
+    return TensorComponents(component, Kind.None(), t.rank - 1, dims)
 end
-function Tensor{3}(curl::Curl, v::Vec{3})
-    return Vec{3}(curl.op[2](v[3]) - curl.op[3](v[2]),
-                  curl.op[3](v[1]) - curl.op[1](v[3]),
-                  curl.op[1](v[2]) - curl.op[2](v[1]))
+function evaluate(curl::Curl, args::Tuple{TensorComponents}, dims::Integer)
+    v = only(args)
+    dims == 3 || throw(ArgumentError(LazyString("curl is only supported for 3D vector fields, got dimension ", dims)))
+    return TensorComponents(Kind.None(), 1, dims) do (i,)
+        j, k = mod1(i + 1, 3), mod1(i + 2, 3)
+        curl.op[j](v[k]) - curl.op[k](v[j])
+    end
 end
